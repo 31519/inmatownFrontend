1e85201f4b5b81dbaba34f84239fe4f919ee87e0
31519 <cosrumut31519>
2022-09-17 13:23:27 +0530

    iamge

--- next.config.js
+++ next.config.js
@@ -22,6 +22,6 @@ module.exports = {
   module.exports = {
     // reactStrictMode=true,
     images: {
-      domains: ['localhost', 'https://inmatown.com'],
+      domains: ['localhost', 'inmatown.com'],
     },
   }
