af72e166851d33eae06c3fd48e936332a46b5eda
31519 <cosrumut31519>
2022-09-18 23:27:06 +0530

    gtag document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,11 +11,34 @@ import Script from "next/script";
 import Router from "next/router";
 import nProgress from "nprogress";
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../../lib/ga'
+
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
 Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   // if (pageProps.error) {
   //   return (
   //     <div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,24 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link
